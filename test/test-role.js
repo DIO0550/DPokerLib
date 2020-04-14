112cc4d755ec2380dd6dfef04ed2b1a27fd74889
DIO0550 <diot12345@yahoo.co.jp>
2020-04-14 22:38:25 +0900

    ツーペアのテストを追加。

--- test/test-role.js
+++ test/test-role.js
@@ -8,7 +8,7 @@ var roleModule = rewire("../role.js")
 /**
  * three card test
  */
-describe("role three card", function() {
+describe("test role three card", function() {
     it("not three card no joker", function(){
         let card1 = new Card(CARD_MARK.SPADE, 3)
         let card2 = new Card(CARD_MARK.SPADE, 12) 
@@ -21,5 +21,52 @@ describe("role three card", function() {
         var isThreeCard = roleModule.__get__("isThreeCard")
 
          assert.equal(isThreeCard(hand), true)
+    });
+
+    it("not three card one joker", function(){
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 3)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+
+        let hand = [card1, card2, card3, card4, card5]
+
+        var isThreeCard = roleModule.__get__("isThreeCard")
+
+         assert.equal(isThreeCard(hand), true)
+    });
+
+    it("not three card two joker", function(){
+        let card1 = new Card(CARD_MARK.SPADE, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 3)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+
+        let hand = [card1, card2, card3, card4, card5]
+
+        var isThreeCard = roleModule.__get__("isThreeCard")
+
+        assert.equal(isThreeCard(hand), true)
+    });
+});
+
+/**
+ * three card test
+ */
+describe("test role two paire", function() {
+    it("not two paire no joker", function(){
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 4) 
+        let card3 = new Card(CARD_MARK.HEART, 3)
+        let card4 = new Card(CARD_MARK.CLUB, 2)
+        let card5 = new Card(CARD_MARK.CLUB, 4)
+
+        let hand = [card1, card2, card3, card4, card5]
+
+        var isTwoPair = roleModule.__get__("isTwoPair")
+
+         assert.equal(isTwoPair(hand), true)
     });
 });
