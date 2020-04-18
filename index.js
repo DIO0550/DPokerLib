71c58f18992f5f3e6940269d02806084fa09efcd
DIO0550 <diot12345@yahoo.co.jp>
2020-04-18 22:04:52 +0900

    ライブラリの本体を追加。

---
+++ index.js
@@ -0,0 +1,7 @@
+var card = require('./lib/card.js');
+var deck = require('./lib/deck.js');
+var role = require('./lib/role.js');
+
+module.exports.card = card
+module.exports.deck = deck
+module.exports.role = role 
