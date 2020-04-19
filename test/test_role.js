8a38d1c3ba8dd076a4c989702479aa2632bf4a09
DIO0550 <diot12345@yahoo.co.jp>
2020-04-19 15:23:12 +0900

    全ての役の判定の単体テストを追加。

--- test/test_role.js
+++ test/test_role.js
@@ -1,9 +1,357 @@
-var DPokerLib = require('../index.js');
+import { Card, CARD_MARK, JOKER_CARD_NUMBER } from '../lib/card.js'
+import { role, ROLE_LIST } from '../lib/role.js'
+
 var assert = require('assert');
 
+
 describe("role", function() {
-    it("role royal flush", function(){
-        //console.log(DPokerLib);
+    it("royal flush", function() {
+        let card1 = new Card(CARD_MARK.DIAMOND, 1)
+        let card2 = new Card(CARD_MARK.DIAMOND, 13) 
+        let card3 = new Card(CARD_MARK.DIAMOND, 11)
+        let card4 = new Card(CARD_MARK.DIAMOND, 12)
+        let card5 = new Card(CARD_MARK.DIAMOND, 10)
+
+        let hand = [card1, card2, card3, card4, card5]
+
+
+         assert.equal(role(hand) == ROLE_LIST.ROYAL_FLUSH, true)
     });
 
+    it("five card one joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 7)
+        let card2 = new Card(CARD_MARK.DIAMOND, 7) 
+        let card3 = new Card(CARD_MARK.HEART, 7)
+        let card4 = new Card(CARD_MARK.CLUB, 7)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+
+        let hand = [card1, card2, card3, card4, card5]
+
+        assert.equal(role(hand) == ROLE_LIST.FIVE_CARD, true)
+    });
+
+    it("five card two joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 10)
+        let card2 = new Card(CARD_MARK.DIAMOND, 10) 
+        let card3 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card4 = new Card(CARD_MARK.HEART, 10)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+
+        let hand = [card1, card2, card3, card4, card5]
+
+        assert.equal(role(hand) == ROLE_LIST.FIVE_CARD, true)
+    });
+
+    it("straight flush no joker", function() {
+        let card1 = new Card(CARD_MARK.DIAMOND, 9)
+        let card2 = new Card(CARD_MARK.DIAMOND, 13) 
+        let card3 = new Card(CARD_MARK.DIAMOND, 11)
+        let card4 = new Card(CARD_MARK.DIAMOND, 12)
+        let card5 = new Card(CARD_MARK.DIAMOND, 10)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRATIGHT_FLUSH, true)
+    });
+
+    it("straight flush one joker", function() {
+        let card1 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.DIAMOND, 13) 
+        let card3 = new Card(CARD_MARK.DIAMOND, 11)
+        let card4 = new Card(CARD_MARK.DIAMOND, 12)
+        let card5 = new Card(CARD_MARK.DIAMOND, 1)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRATIGHT_FLUSH, true)
+    });
+
+    it("straight flush two joker", function() {
+        let card1 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.DIAMOND, 13) 
+        let card3 = new Card(CARD_MARK.DIAMOND, 11)
+        let card4 = new Card(CARD_MARK.DIAMOND, 12)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRATIGHT_FLUSH, true)
+    });
+
+    it("four card no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 1)
+        let card2 = new Card(CARD_MARK.DIAMOND, 13) 
+        let card3 = new Card(CARD_MARK.HEART, 1)
+        let card4 = new Card(CARD_MARK.DIAMOND, 1)
+        let card5 = new Card(CARD_MARK.CLUB, 1)
+
+        let hand = [card1, card2, card3, card4, card5]
+
+        assert.equal(role(hand) == ROLE_LIST.FOUR_CARD, true)
+    });
+
+    it("four card one joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 6)
+        let card2 = new Card(CARD_MARK.SPADE, 13) 
+        let card3 = new Card(CARD_MARK.HEART, 6)
+        let card4 = new Card(CARD_MARK.CLUB, 6)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.FOUR_CARD, true)
+    });
+
+    it("four card two joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 10)
+        let card2 = new Card(CARD_MARK.DIAMOND, 5) 
+        let card3 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card4 = new Card(CARD_MARK.HEART, 10)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.FOUR_CARD, true)
+    });
+
+    it("full hause no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 1)
+        let card2 = new Card(CARD_MARK.SPADE, 13) 
+        let card3 = new Card(CARD_MARK.HEART, 1)
+        let card4 = new Card(CARD_MARK.CLUB, 13)
+        let card5 = new Card(CARD_MARK.CLUB, 1)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.FULL_HAUSE, true)
+    });
+
+    it("full hause one joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 6)
+        let card2 = new Card(CARD_MARK.SPADE, 13) 
+        let card3 = new Card(CARD_MARK.HEART, 6)
+        let card4 = new Card(CARD_MARK.CLUB, 13)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.FULL_HAUSE, true)
+    });
+
+    it("flush no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.SPADE, 6)
+        let card4 = new Card(CARD_MARK.SPADE, 7)
+        let card5 = new Card(CARD_MARK.SPADE, 10)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.FLUSH, true)
+    });
+
+    it("flush one joker", function() {
+        let card1 = new Card(CARD_MARK.HEART, 3)
+        let card2 = new Card(CARD_MARK.HEART, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.HEART, 4)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.FLUSH, true)
+    });
+
+    it("flush two joker", function() {
+        let card1 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.CLUB, 12) 
+        let card3 = new Card(CARD_MARK.CLUB, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 3)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.FLUSH, true)
+    });
+
+    it("straight no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 4) 
+        let card3 = new Card(CARD_MARK.HEART, 7)
+        let card4 = new Card(CARD_MARK.CLUB, 6)
+        let card5 = new Card(CARD_MARK.CLUB, 5)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+
+    it("straight no joker 10 ~ 1", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 1)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 13)
+        let card4 = new Card(CARD_MARK.CLUB, 10)
+        let card5 = new Card(CARD_MARK.CLUB, 11)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        console.log(role(hand))
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+
+    it("straight one joker 9 ~ 13", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 10)
+        let card2 = new Card(CARD_MARK.SPADE, 9) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 13)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+
+    it("straight one joker 10 ~ 12 1", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 12)
+        let card2 = new Card(CARD_MARK.SPADE, 1) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 10)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+
+    it("straight two joker 11 ~ 12 1", function() {
+        let card1 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 1)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+    
+    it("straight two joker 12 ~ 13 1", function() {
+        let card1 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 13)
+        let card4 = new Card(CARD_MARK.CLUB, 1)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+    
+    
+    it("straight two joker 5 ~ 6 8", function() {
+        let card1 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.SPADE, 8) 
+        let card3 = new Card(CARD_MARK.HEART, 5)
+        let card4 = new Card(CARD_MARK.CLUB, 6)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+    
+    it("straight two joker 6 ~ 7 10", function() {
+        let card1 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.SPADE, 10) 
+        let card3 = new Card(CARD_MARK.HEART, 6)
+        let card4 = new Card(CARD_MARK.CLUB, 7)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.STRAIGHT, true)
+    });
+
+    it("three card no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 3)
+        let card4 = new Card(CARD_MARK.CLUB, 3)
+        let card5 = new Card(CARD_MARK.CLUB, 6)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.THREE_CARD, true)
+    });
+
+    it("three card one joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 3)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.THREE_CARD, true)
+    });
+
+    it("three card two joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, JOKER_CARD_NUMBER)
+        let card2 = new Card(CARD_MARK.SPADE, 12) 
+        let card3 = new Card(CARD_MARK.HEART, 11)
+        let card4 = new Card(CARD_MARK.CLUB, 3)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.THREE_CARD, true)
+    });
+
+    it("two pair no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 4) 
+        let card3 = new Card(CARD_MARK.HEART, 3)
+        let card4 = new Card(CARD_MARK.CLUB, 2)
+        let card5 = new Card(CARD_MARK.CLUB, 4)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.TWO_PAIR, true)
+    });
+
+    it("one pair no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 5) 
+        let card3 = new Card(CARD_MARK.HEART, 3)
+        let card4 = new Card(CARD_MARK.CLUB, 2)
+        let card5 = new Card(CARD_MARK.CLUB, 4)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.ONE_PAIR, true)
+    });
+
+    it("one pair one joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 5) 
+        let card3 = new Card(CARD_MARK.HEART, 10)
+        let card4 = new Card(CARD_MARK.CLUB, 2)
+        let card5 = new Card(CARD_MARK.JOKER, JOKER_CARD_NUMBER)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.ONE_PAIR, true)
+    });
+
+    it("high card no joker", function() {
+        let card1 = new Card(CARD_MARK.SPADE, 3)
+        let card2 = new Card(CARD_MARK.SPADE, 10) 
+        let card3 = new Card(CARD_MARK.HEART, 6)
+        let card4 = new Card(CARD_MARK.CLUB, 7)
+        let card5 = new Card(CARD_MARK.CLUB, 4)
+        
+        let hand = [card1, card2, card3, card4, card5]
+        
+        assert.equal(role(hand) == ROLE_LIST.HIGH_CARD, true)
+    });
 });
